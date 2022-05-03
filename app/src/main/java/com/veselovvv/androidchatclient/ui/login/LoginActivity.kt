c62bf9f40d6bb901f89942fdb75b2a73f9bc9340
veselovvv <vycheslav5veselov@gmail.com>
2022-05-03 14:23:03 +0300

    Added Login Activity

---
+++ app/src/main/java/com/veselovvv/androidchatclient/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.veselovvv.androidchatclient.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.veselovvv.androidchatclient.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container_login, LoginFragment())
+            .commit()
+    }
+}
